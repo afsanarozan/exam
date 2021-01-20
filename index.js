1d65199a4d1042d97e281e90ceeb8676e8f9071a
afsanarozan <afsanarozannaufal9@gmail.com>
2021-01-20 14:28:44 +0700

    tes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.options('*', cors()) // include before other routes
 app.use(cors())
 
 app.get("/", (req, res) => {
-	return res.json({data: "ok" });
+	return res.json({data: "connected" });
 });
 
 app.get("/ping", (req, res) => {
